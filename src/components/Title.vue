f15a9511a381c956ee3f1532e2b318512c823486
alemica97 <alessandro.micalizzi08@gmail.com>
2022-03-07 18:08:46 +0100

    Ho creato un componente che ho importato nell'App.vue che contiene un titolo

---
+++ src/components/Title.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="title">
+        <h1>{{ pageTitle }}</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'myTitle',
+
+    data(){
+        return{
+            pageTitle : 'La mia prima app con Vue CLI!',
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
